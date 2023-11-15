6d8cbc42d67e59e10717768b25d159d866af0b7c
Chadfire <chadfire10@yahoo.com>
2023-11-15 12:48:26 -0500

    Created footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/NavigationBar';
 import Home from './pages/Home';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
